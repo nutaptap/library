ef8f5a553f9dfd0c58459ed2de9f84852b2fe6e4
nutaptap <nutaptap@gmail.com>
2022-09-26 16:18:54 +0200

    Added functions to add and remove books

--- script.js
+++ script.js
@@ -31,40 +31,71 @@ document.addEventListener("keyup", (event) => {
   }
 });
 
+// GET THE FORM DATA
+
+function getData() {
+  const getTitle = document.getElementById("title").value;
+  const getAuthor = document.getElementById("author").value;
+  const getPages = document.getElementById("pages").value;
+  const getRead = document.getElementById("read").checked;
+  createNewCard(getTitle, getAuthor, getPages, getRead);
+  makeInvisible();
+}
+
 // LIBRARY
 
 const library = document.getElementById("cards-container");
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-}
-
-let placeholderTitle = "James Potates";
-let placeholderAuthor = "My potato";
-let placeholderPages = "434";
-
-function createNewCard() {
+function createNewCard(title, author, pages, read) {
   const newCard = document.createElement("div");
   newCard.setAttribute("class", "card");
   newCard.innerHTML = `
-  <button class="card-delete"
+  <button onClick="this.parentNode.parentNode.removeChild(this.parentNode);" class="card-delete" id="card-delete"
           ><img class="delete-image" src="delete.svg"
         /></button>
-        <div class="card-title"><h3>${placeholderTitle}</h3></div>
-        <div class="card-author"><p>${placeholderAuthor}</p></div>
-        <div class="card-pages"><p>${placeholderPages} Pages</p></div>
-        <div class="card-read"
-          ><p>Read</p
-          ><button class="read-button"
-            ><img class="check-image" src="check-filled.svg" /></button
-        ></div>
+        <div class="card-title"><h3>${title}</h3></div>
+        <div class="card-author"><p>${author}</p></div>
+        <div class="card-pages"><p>${pages} Pages</p></div>
+        <button class="read-button" id="read-button"
+          >Read <img class="check-image" src="check-filled.svg"
+        /></button>
 `;
   library.appendChild(newCard);
 }
 
-createNewCard();
+createNewCard(
+  "Harry Potter and the Prisioner of Azkaban",
+  "J.K. Rowling",
+  "317",
+  true
+);
 
-function addNewBook() {}
+createNewCard("Atomic Habits", "James Clear", "285", true);
+
+createNewCard(
+  "Harry Potter and the Prisioner of Azkaban",
+  "J.K. Rowling",
+  "317",
+  true
+);
+
+createNewCard(
+  "Harry Potter and the Prisioner of Azkaban",
+  "J.K. Rowling",
+  "317",
+  true
+);
+
+createNewCard(
+  "Harry Potter and the Prisioner of Azkaban",
+  "J.K. Rowling",
+  "317",
+  true
+);
+
+createNewCard(
+  "Harry Potter and the Prisioner of Azkaban",
+  "J.K. Rowling",
+  "317",
+  true
+);
